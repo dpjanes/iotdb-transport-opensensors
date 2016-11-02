a0f2506a3226d13578ed20ed78b853a05d0c18d6
David Janes <davidjanes@davidjanes.com>
2016-11-02 08:53:05 -0400

    initial checkin

---
+++ DIST.sh
@@ -0,0 +1,48 @@
+#
+#   DIST.sh
+#
+#   David Janes
+#   IOTDB
+#   2016-11-02
+#
+
+exit 0
+
+PACKAGE=iotdb-transport-opensensors
+DIST_ROOT=/var/tmp/.dist.$$
+
+if [ ! -d "$DIST_ROOT" ]
+then
+    mkdir "$DIST_ROOT"
+fi
+
+echo "=================="
+echo "NPM Packge: $PACKAGE"
+echo "=================="
+(
+    NPM_DST="$DIST_ROOT/$PACKAGE"
+    echo "NPM_DST=$NPM_DST"
+
+    if [ -d ${NPM_DST} ]
+    then
+        rm -rf "${NPM_DST}"
+    fi
+    mkdir "${NPM_DST}" || exit 1
+
+    update-package --increment-version --package "$PACKAGE" --homestar || exit 1
+
+    tar cf - \
+        --exclude "node_modules" \
+        --exclude "xxx*" \
+        --exclude "yyy*" \
+        --exclude "Gruntfile.js" \
+        README.md LICENSE \
+        homestar.json package.json \
+        *.js \
+        |
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
+
+    echo "end"
+)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# iotdb-transport-opensensors
-IOTDB Transporter for OpenSensors.io
+# iotdb-transport-prototype
+[IOTDB](https://github.com/dpjanes/node-iotdb) 
+[Transporter](https://github.com/dpjanes/node-iotdb/blob/master/docs/transporters.md)
+for
+[OpenSensors.io](https://www.opensensors.io/)
+
+<img src="https://raw.githubusercontent.com/dpjanes/iotdb-homestar/master/docs/HomeStar.png" align="right" />
+
+# About
+
+* [Read more about Transporters](https://github.com/dpjanes/node-iotdb/blob/master/docs/transporters.md)
+* [OpenSensors.io](https://www.opensensors.io/)
+
